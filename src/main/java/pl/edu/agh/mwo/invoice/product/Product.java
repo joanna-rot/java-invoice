4653896ea62e630101c7cddbaad55713a9229e90
Joanna Zajac-Szymura <jzajacszymura@gmail.com>
2023-02-11 15:24:07 +0100

    All tests of Products

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,14 +10,26 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Nazwa nie moze byc nullem");
+        }
+// or if (name == null || name == "") {
+//            throw new IllegalArgumentException("Nazwa nie moze byc nullem i pusta");
+//        }
+//        if (name.isEmpty()) {
+//            throw new IllegalArgumentException("Nazwa nie moze byc pusta");
+//        }
+
+        if (price == null || price.compareTo(BigDecimal.ZERO) == -1 ) {
+            throw new IllegalArgumentException("Cena nie moze byc nullem i nie moze byc ujemna");
+        }
+
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
-    public String getName() {
-        return this.name;
-    }
+    public String getName() {return this.name;}
 
     public BigDecimal getPrice() { return this.price;}
 
